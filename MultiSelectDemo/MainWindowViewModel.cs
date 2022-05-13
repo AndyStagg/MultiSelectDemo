8fbcf68064d65cc4083bfbb6098249ec343ceee7
andy stagg <atwinda@gmail.com>
2022-05-13 16:33:22 -0700

    Add project files.

---
+++ MultiSelectDemo/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MultiSelectDemo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            MyItems = new ObservableCollection<MyItem>();
+            MyItems.Add(new MyItem() { Property_One = "One", Property_Two = "Two", Property_Three = "Three" });
+            MyItems.Add(new MyItem() { Property_One = "One", Property_Two = "Two", Property_Three = "Three" });
+            MyItems.Add(new MyItem() { Property_One = "One", Property_Two = "Two", Property_Three = "Three" });
+            MyItems.Add(new MyItem() { Property_One = "One", Property_Two = "Two", Property_Three = "Three" });
+        }
+
+        private ObservableCollection<MyItem> _myItems;
+        public ObservableCollection<MyItem> MyItems
+        {
+            get => _myItems;
+            set => Set(ref _myItems, value);
+        }
+
+        private IList<MyItem> _mySelectedItems;
+        public IList<MyItem> MySelectedItems
+        {
+            get => _mySelectedItems;
+            set
+            {
+                Set(ref _mySelectedItems, value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MultiSelectDemo/MyItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiSelectDemo
+{
+    public class MyItem
+    {
+        public string Property_One { get; set; }
+        public string Property_Two { get; set; }
+        public string Property_Three { get; set; }
+    }
+}
